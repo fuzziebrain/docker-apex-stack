b0a0b9a22bc999e71e280c82cc776729b3eef9ec
Adrian Png <adrian.png@fuzziebrain.com>
2019-02-16 10:29:42 -0800

    Setting oradata ownership

--- 03-run.sh
+++ 03-run.sh
@@ -3,8 +3,17 @@
 CONTAINER_NAME=${1:-axer}
 ENV_FILE=${2:-.env}
 
+echo "##### Removing any previous containers #####"
 docker rm -vf $CONTAINER_NAME
 
+if [ ! -d "oradata" ]; then
+  mkdir oradata
+fi
+
+echo "##### Changing file ownership. May require password to continue. #####"
+sudo chown 54321:54321 oradata
+
+echo "##### Creating container $CONTAINER_NAME #####"
 docker run -d --name $CONTAINER_NAME \
         -p 50080:8080 \
         -p 5500:5500 \
@@ -16,4 +25,5 @@ docker run -d --name $CONTAINER_NAME \
         -v $PWD/files:/tmp/files \
         oracle/database:18.4.0-xe
 
+echo "##### Tailing logs. Ctrl-C to exit. #####"
 docker logs -f $CONTAINER_NAME
