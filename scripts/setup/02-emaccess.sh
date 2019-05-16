932b3dce735805a8861c42cdaae3da2a566cbbba
Adrian Png <adrian.png@fuzziebrain.com>
2019-05-15 21:10:13 -0700

    Fix for issue #9

--- scripts/setup/02-emaccess.sh
+++ scripts/setup/02-emaccess.sh
@@ -2,7 +2,7 @@
 
 export SCRIPT_DIR=$SCRIPTS_ROOT
 
-if [ "$DB_VERSION" = "18.4.0" ] && [ "$DB_EDITION" = "XE" ]; then
+if [ "$DB_VERSION" = "18.4.0" ] && [ "$DB_EDITION" = "xe" ]; then
   echo "##### Enable EM Access for ${DB_VERSION} ${DB_EDITION} #####"
   runuser oracle -m -s /bin/bash -c ". $SCRIPT_DIR/package/enableEmAccess.sh"
 fi
