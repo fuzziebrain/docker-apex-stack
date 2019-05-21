cf17558c5a0a0084e086528a5ef3939a95b4be66
Adrian Png <adrian.png@fuzziebrain.com>
2019-05-21 20:30:22 +0000

    Resolve issue #12

--- 02-run.sh
+++ 02-run.sh
@@ -9,6 +9,12 @@ BASE_DIR=$(pwd -P)
 DB_VERSION=${DB_VERSION:-18.4.0}
 DB_EDITION=$(echo ${DB_EDITION:-xe} | tr '[:upper:]' '[:lower:]')
 HOST_DATA_DIR=${CONTAINER_NAME}-oradata
+DOCKER_NETWORK_NAME=${DOCKER_NETWORK_NAME:-bridge}
+
+echo "##### Check if Docker network $DOCKER_NETWORK_NAME #####"
+docker network inspect -f {{.Name}} $DOCKER_NETWORK_NAME || \
+  echo "##### Create Docker network $DOCKER_NETWORK_NAME #####"; \
+  docker network create $DOCKER_NETWORK_NAME 
 
 echo "##### Removing any previous containers #####"
 docker rm -vf $CONTAINER_NAME
@@ -22,6 +28,7 @@ sudo -n chown 54321:543321 ${HOST_DATA_DIR} || chmod 777 ${HOST_DATA_DIR}
 
 echo "##### Creating container $CONTAINER_NAME #####"
 docker run -d --name $CONTAINER_NAME \
+        --network ${DOCKER_NETWORK_NAME} \
         -p ${DOCKER_ORDS_PORT:-50080}:8080 \
         -p ${DOCKER_EM_PORT:-55500}:5500 \
         -p ${DOCKER_DB_PORT:-51521}:1521 \
