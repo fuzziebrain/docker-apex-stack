18154b71427a70646017bcfb69b8356152eea11a
Adrian Png <adrian.png@fuzziebrain.com>
2023-01-01 16:19:23 -0800

    Fix for #67.

--- scripts/setup/01-install.sh
+++ scripts/setup/01-install.sh
@@ -4,37 +4,31 @@ export APEX_HOME=$ORACLE_BASE/product/apex
 export ORDS_HOME=$ORACLE_BASE/product/ords
 export SCRIPT_DIR=$SCRIPTS_ROOT
 export FILES_DIR=/tmp/files
+export JAVA_HOME=$ORACLE_BASE/product/java/latest
+export PATH=$JAVA_HOME/bin:$PATH
+
+case "${INSTALL_FILE_JAVA}" in
+  openjdk11) DOWNLOAD_URL=https://download.java.net/java/ga/jdk11/openjdk-11_linux-x64_bin.tar.gz;;
+  *.tar.gz) ;;
+  *) DOWNLOAD_URL=https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz;;
+esac
+
+if [ ! -z "${DOWNLOAD_URL}" ]; then
+  export INSTALL_FILE_JAVA="${DOWNLOAD_URL##*/}"
+fi
 
 echo "##### Install dependencies if required #####"
-if [ ! $(command -v java) ]; then
-  if [[ $UID == "0" && $INSTALL_FILE_JAVA == 'java17' ]]; then
-    yum install -y https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm
-  elif [[ $UID == "0" && $INSTALL_FILE_JAVA == 'openjdk1.8' ]]; then
-    yum install -y java-1.8.0-openjdk
-  elif [[ $UID == "0" && $INSTALL_FILE_JAVA == 'openjdk11' ]]; then
-    yum install -y java-11-openjdk
-  else
-    export JAVA_HOME=$ORACLE_BASE/product/java/latest
-    export PATH=$JAVA_HOME/bin:$PATH
-
-    if [ ! -d $JAVA_HOME ]; then
-      if [[ ! $FILENAME =~ .tar.gz$ ]]; then
-        INSTALL_FILE_JAVA_DEFAULT="jdk-17_linux-x64_bin.tar.gz"
-        curl https://download.oracle.com/java/17/latest/$INSTALL_FILE_JAVA_DEFAULT \
-          --output /tmp/$INSTALL_FILE_JAVA_DEFAULT
-        JAVA_DIR_NAME=`tar -tzf /tmp/$INSTALL_FILE_JAVA_DEFAULT | head -1 | cut -f1 -d"/"`
-        mkdir -p $ORACLE_BASE/product/java
-        tar zxf /tmp//$INSTALL_FILE_JAVA_DEFAULT --directory $ORACLE_BASE/product/java
-        ln -s $ORACLE_BASE/product/java/$JAVA_DIR_NAME $JAVA_HOME
-        rm -f /tmp/$INSTALL_FILE_JAVA_DEFAULT
-      else
-        JAVA_DIR_NAME=`tar -tzf $FILES_DIR/$INSTALL_FILE_JAVA | head -1 | cut -f1 -d"/"`
-        mkdir -p $ORACLE_BASE/product/java
-        tar zxf $FILES_DIR/$INSTALL_FILE_JAVA --directory $ORACLE_BASE/product/java
-        ln -s $ORACLE_BASE/product/java/$JAVA_DIR_NAME $JAVA_HOME
-      fi
-    fi
+if [ ! -d $JAVA_HOME ]; then
+  if [ ! -f "$FILES_DIR/$INSTALL_FILE_JAVA" ]; then
+    curl --output $FILES_DIR/$INSTALL_FILE_JAVA ${DOWNLOAD_URL}
   fi
+  JAVA_DIR_NAME=`tar -tzf $FILES_DIR/$INSTALL_FILE_JAVA | head -1 | cut -f1 -d"/"`
+  mkdir -p $ORACLE_BASE/product/java
+  tar zxf $FILES_DIR/$INSTALL_FILE_JAVA --directory $ORACLE_BASE/product/java
+  ln -s $ORACLE_BASE/product/java/$JAVA_DIR_NAME $JAVA_HOME
+
+  echo -e 'export JAVA_HOME=$ORACLE_BASE/product/java/latest' >> $HOME/.bashrc
+  echo -e 'export PATH=$JAVA_HOME/bin:$PATH' >> $HOME/.bashrc
 fi
 
 if [[ $OML4R_SUPPORT =~ (Y|y) ]]; then
